19b6fe669240bdcf68de77be5ce4a7456f6a6624
Alex Visbal <84116246+avisbal23@users.noreply.github.com>
2023-06-12 03:56:47 +0000

    sid

--- sendText.py
+++ sendText.py
@@ -1,14 +1,23 @@
 #pip install --user --upgrade twilio 
-
+# Download the helper library from https://www.twilio.com/docs/python/install
 from twilio.rest import Client
-accountSID = 'ACe0f26e6158438ffd7c1cea2b4f79b654'
-authToken = '320201e44ff54d3723e29481254fc8ad'
-myTwilioNumber = "+18445310674"
-myCellPhone = "+16786659558"
 
-client = Client(accountSID, authToken)
+#Set Credentials
+account_sid = "ACe0f26e6158438ffd7c1cea2b4f79b654"
+auth_token = "67c9da3bfe7ab48ffc8f59ae2fee2514"
+
+# create message client object 
+client = Client(account_sid, auth_token)
+
+# Set message content 
+smsContent = '''...
+
+Hi, I love you. so much. 
+Te amo - good night. <3'''
+# Send message 
 message = client.messages.create(
-  body="Yo yo yo!",
-  from_=myTwilioNumber,
-  to=myCellPhone
+  body=smsContent,
+  from_="+18445310674",
+  to="+16786659558"
 )
+print(message.body)
